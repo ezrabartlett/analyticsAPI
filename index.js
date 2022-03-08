caa6508c6a823908cbef1577fe4440f4e1ad7dde
Ezra Bartlett <ezra_bartlett@homedepot.com>
2022-03-08 12:56:17 -0600

    Log system generation

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new Client({
 
 client.connect();
 
-const getDateTime = {
+const getDateTime = () => {
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
